f3cb1d906d5271281c261cf7ca7a9a807618db6e
NorthernMan54 <buyminivan-northern@yahoo.ca>
2021-01-25 23:05:36 -0500

    Hopping working

--- main/main.ino
+++ main/main.ino
@@ -10,12 +10,15 @@
 
 float frequencies[] = {303.79, 315.026, 433.92};
 
+byte FSCAL1[3];
+byte FSCAL2[3];
+byte FSCAL3[3];
+
 int start_freq = 0;
 int stop_freq = 2;
 
 int freq = start_freq;
 int compare_freq;
-int mark_freq;
 int rssi;
 int mark_rssi = -100;
 
@@ -25,12 +28,29 @@ unsigned long receiveEnd = millis();
 RCSwitch rcSwitch = RCSwitch();
 ESPiLight piLight(-1); // use -1 to disable transmitter
 
+byte radioState = 0;
+
 static inline void safeDelayMicroseconds(unsigned long duration)
 {
-
   // if delay > 10 milliseconds, use yield() to avoid wdt reset
   unsigned long start = micros();
+  // Log.notice(F("Radio State: %d" CR), radioState);
   while ((micros() - start) < duration)
+  {
+    if (radioState != ELECHOUSE_cc1101.SpiReadStatus(CC1101_MARCSTATE))
+    {
+      Log.notice(F("Time: %d" CR), micros() - start);
+      radioState = ELECHOUSE_cc1101.SpiReadStatus(CC1101_MARCSTATE);
+      Log.notice(F("Radio State: %d" CR), radioState);
+    }
+
+    yield();
+  }
+}
+
+void waitForRadioReady()
+{
+  while (ELECHOUSE_cc1101.SpiReadStatus(CC1101_MARCSTATE) != 13)
   {
     yield();
   }
@@ -41,14 +61,74 @@ void pilightCallback(const String &protocol, const String &message, int status,
 {
   if (status == VALID)
   {
-    Log.trace(F("Creating RF PiLight buffer" CR));
+    Log.notice(F("time: %d" CR), millis() / 1000);
     Log.notice(F("value: %s" CR), (char *)message.c_str());
     Log.notice(F("protocol: %s" CR), (char *)protocol.c_str());
     Log.notice(F("length: %s" CR), (char *)deviceID.c_str());
-    Log.notice(F("Freq: %F" CR), frequencies[mark_freq]);
+//     Log.notice(F("Freq: %F" CR), frequencies[freq]);
     Log.notice(F("RSSI: %d" CR), ELECHOUSE_cc1101.getRssi());
     Log.notice(F("Time: %d" CR), receiveEnd - millis());
   }
+}
+
+void calibrate()
+{
+  for (int freq = 0; freq <= 2; freq++)
+  {
+    ELECHOUSE_cc1101.SetRx(frequencies[freq]);
+    waitForRadioReady();
+    safeDelayMicroseconds(300);
+    FSCAL1[freq] = ELECHOUSE_cc1101.SpiReadStatus(CC1101_FSCAL1);
+    FSCAL2[freq] = ELECHOUSE_cc1101.SpiReadStatus(CC1101_FSCAL2);
+    FSCAL3[freq] = ELECHOUSE_cc1101.SpiReadStatus(CC1101_FSCAL3);
+    Log.notice(F("Calibrated: %F" CR), frequencies[freq]);
+    Log.notice(F("FSCAL1: %d" CR), FSCAL1[freq]);
+    Log.notice(F("FSCAL2: %d" CR), FSCAL2[freq]);
+    Log.notice(F("FSCAL3: %d" CR), FSCAL3[freq]);
+  }
+}
+
+void setMHZ(float mhz)
+{
+  byte freq2 = 0;
+  byte freq1 = 0;
+  byte freq0 = 0;
+
+  float MHz = mhz;
+
+  for (bool i = 0; i == 0;)
+  {
+    if (mhz >= 26)
+    {
+      mhz -= 26;
+      freq2 += 1;
+    }
+    else if (mhz >= 0.1015625)
+    {
+      mhz -= 0.1015625;
+      freq1 += 1;
+    }
+    else if (mhz >= 0.00039675)
+    {
+      mhz -= 0.00039675;
+      freq0 += 1;
+    }
+    else
+    {
+      i = 1;
+    }
+  }
+  if (freq0 > 255)
+  {
+    freq1 += 1;
+    freq0 -= 256;
+  }
+
+  ELECHOUSE_cc1101.SpiWriteReg(CC1101_FREQ2, freq2);
+  ELECHOUSE_cc1101.SpiWriteReg(CC1101_FREQ1, freq1);
+  ELECHOUSE_cc1101.SpiWriteReg(CC1101_FREQ0, freq0);
+
+  // Calibrate();
 }
 
 void setup()
@@ -64,6 +144,11 @@ void setup()
 
   Serial.println();
   Serial.println("Frequency Scanner Ready");
+
+#ifdef HOPPING
+  calibrate();
+  ELECHOUSE_cc1101.SpiWriteReg(CC1101_MCSM0, ELECHOUSE_cc1101.SpiReadReg(CC1101_MCSM0) & 0x0F);
+#endif
 }
 
 void loop()
@@ -71,62 +156,57 @@ void loop()
 
   if (!receiveMode)
   {
+    unsigned long start = micros();
+#ifdef HOPPING
+    ELECHOUSE_cc1101.SpiStrobe(CC1101_SIDLE);
+    setMHZ(frequencies[freq]);
+    ELECHOUSE_cc1101.SpiWriteReg(CC1101_FSCAL1, FSCAL1[freq]);
+    ELECHOUSE_cc1101.SpiWriteReg(CC1101_FSCAL2, FSCAL2[freq]);
+    ELECHOUSE_cc1101.SpiWriteReg(CC1101_FSCAL3, FSCAL3[freq]);
+#else
     ELECHOUSE_cc1101.SetRx(frequencies[freq]);
-    // Serial.print(ELECHOUSE_cc1101.SpiReadStatus(CC1101_MARCSTATE));
-    // Serial.print("-");
-    safeDelayMicroseconds(1300);
+#endif
+    ELECHOUSE_cc1101.SpiStrobe(CC1101_SRX);
+    waitForRadioReady();
+    safeDelayMicroseconds(200);
     rssi = ELECHOUSE_cc1101.getRssi();
-    // Serial.print(ELECHOUSE_cc1101.SpiReadStatus(CC1101_MARCSTATE));
-    // Serial.print(",");
-    if (rssi > -200)
+
+    if (rssi > -70)
     {
-      if (rssi > mark_rssi)
+
+      Serial.println();
+      Log.notice(F("Freq: %F" CR), frequencies[freq]);
+      Log.notice(F("RSSI: %d" CR), ELECHOUSE_cc1101.getRssi());
+
+      switch (freq)
       {
-        // Serial.print(".");
-        mark_rssi = rssi;
-        mark_freq = freq;
+      case 0: // RC Switch
+      case 1: // RC Switch
+        Log.notice(F("RC Switch Enabled: %d" CR), RF_RECEIVER_GPIO);
+        rcSwitch.enableReceive(RF_RECEIVER_GPIO);
+        receiveMode = true;
+        receiveEnd = millis() + 300; // Max signal length
+        break;
+      case 2:
+        Log.notice(F("PiLight Enabled: %d" CR), RF_RECEIVER_GPIO);
+        piLight.enableReceiver();
+        piLight.initReceiver(RF_RECEIVER_GPIO);
+        receiveMode = true;
+        receiveEnd = millis() + 300; // Max signal length
+        break;
+      default:
+        Log.notice(F("Unhandled Frequency: %d %f" CR), freq, frequencies[freq]);
+        break;
       }
     }
-
-    freq += 1;
-
-    if (freq > stop_freq)
+    else
     {
-      // Serial.println();
-      freq = start_freq;
+      freq += 1;
 
-      if (mark_rssi > -70)
+      if (freq > stop_freq)
       {
-
-        Serial.println();
-        Serial.print("Freq: ");
-        Serial.println(frequencies[mark_freq]);
-        Serial.print("Rssi: ");
-        Serial.println(mark_rssi);
-        mark_rssi = -200;
-
-        switch (mark_freq)
-        {
-        case 0: // RC Switch
-        case 1: // RC Switch
-          Log.notice(F("RC Switch Enabled: %d" CR), RF_RECEIVER_GPIO);
-          ELECHOUSE_cc1101.SetRx(frequencies[mark_freq]);
-          rcSwitch.enableReceive(RF_RECEIVER_GPIO);
-          receiveMode = true;
-          receiveEnd = millis() + 800; // Max signal length
-          break;
-        case 2:
-          Log.notice(F("PiLight Enabled: %d" CR), RF_RECEIVER_GPIO);
-          // ELECHOUSE_cc1101.SetRx(frequencies[mark_freq]);
-          piLight.enableReceiver();
-          piLight.initReceiver(RF_RECEIVER_GPIO);
-          receiveMode = true;
-          receiveEnd = millis() + 800; // Max signal length
-          break;
-        default:
-          Log.notice(F("Unhandled Frequency: %d %f" CR), mark_freq, frequencies[mark_freq]);
-          break;
-        }
+        // Serial.println();
+        freq = start_freq;
       }
     }
   }
@@ -147,14 +227,14 @@ void loop()
 
   if (rcSwitch.available())
   {
-    Log.trace(F("RF Task running on core :%d" CR), xPortGetCoreID());
+    Log.notice(F("time: %d" CR), millis() / 1000);
     Log.notice(F("value: %u" CR), (unsigned long)rcSwitch.getReceivedValue());
     Log.notice(F("protocol: %d" CR), (int)rcSwitch.getReceivedProtocol());
     Log.notice(F("length: %d" CR), (int)rcSwitch.getReceivedBitlength());
-    Log.notice(F("delay: %d" CR), (int)rcSwitch.getReceivedDelay());
-    Log.notice(F("Freq: %F" CR), frequencies[mark_freq]);
+    // Log.notice(F("delay: %d" CR), (int)rcSwitch.getReceivedDelay());
+    // Log.notice(F("Freq: %F" CR), frequencies[freq]);
     Log.notice(F("RSSI: %d" CR), ELECHOUSE_cc1101.getRssi());
-      Log.notice(F("Time: %d" CR), receiveEnd - millis());
+    Log.notice(F("Time: %d" CR), receiveEnd - millis());
 
     rcSwitch.resetAvailable();
     rcSwitch.disableReceive();
